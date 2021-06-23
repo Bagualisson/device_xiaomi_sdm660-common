16001dab083fa26335683837dc5ee5f6d4c5c7a7
AlissonGrizotti <alisson1397@hotmail.com>
2021-06-23 20:51:04 -0300

    sdm660-common: Decommonize android.hardware.usb

--- sdm660.mk
+++ sdm660.mk
@@ -465,10 +465,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
-
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator@1.2-service
